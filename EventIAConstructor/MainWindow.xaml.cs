f1ad8a3ba2f76b0a71c136c5b21a564f2d4072ec
Konctantin <gawrilyako@gmail.com>
2015-10-10 09:36:14 +0300

    upd
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

--- EventIAConstructor/MainWindow.xaml.cs
+++ EventIAConstructor/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using System.Windows;
 
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using EventIAConstructor.EventAI.ViewModel;
+using EventIAConstructor.EventAI.Metadata;
+
 namespace EventIAConstructor
 {
     /// <summary>
@@ -11,11 +16,42 @@ namespace EventIAConstructor
         {
             InitializeComponent();
 
-            cbFlags.FlagsSource = typeof(EventAI.Metadata.EventFlags);
-            cbFlags.SelectedFlag = 5;
+            using (var conn = new MySqlConnection("server=localhost;user id=root; password=mangos; database=mangos; pooling=false"))
+            {
+                conn.Open();
+                using (var command = new MySqlCommand("select * from creature_ai_scripts", conn))
+                {
+                    var reader = command.ExecuteReader();
+                    EventAIDataBase.EventAIList.Clear();
+                    while (reader.Read())
+                    {
+                        var ai = new EventAIModel(
+                            reader.GetInt32(0),
+                            reader.GetInt32(1),
+                            reader.GetInt32(3),
+                            reader.GetInt32(4),
+                            reader.GetInt32(5)
+                            );
+                        var ev = new EventModel(ai, reader.GetInt32(2),
+                            reader.GetInt32(6), reader.GetInt32(7), reader.GetInt32(8), reader.GetInt32(9));
+                        var ac = new ActionModel[3];
+                        for (int i = 0, j = 0; i < ac.Length; ++i, j+=4)
+                        {
+                            ac[i] = new ActionModel(ai, reader.GetInt32(10+j),
+                                reader.GetInt32(11 + j), reader.GetInt32(12 + j), reader.GetInt32(13 + j));
+                        }
+
+                        ai.Event = ev;
+                        ai.Action1 = ac[0];
+                        ai.Action2 = ac[1];
+                        ai.Action3 = ac[2];
+                        EventAIDataBase.EventAIList.Add(ai);
+                    }
+                }
+            }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        void Button_Click(object sender, RoutedEventArgs e)
         {
         }
     }
